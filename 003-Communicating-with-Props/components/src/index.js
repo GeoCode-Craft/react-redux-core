a515b168bfd1cf47fdd4b56acad172a005c83f22
PondiB <pondibrian@gmail.com>
2020-10-28 16:01:05 +0100

    :sparkles: React component reuse and props children

--- 003-Communicating-with-Props/components/src/index.js
+++ 003-Communicating-with-Props/components/src/index.js
@@ -2,12 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import CommentDetail from './CommentDetail'
 import faker from 'faker'
+import ApprovalCard from './ApprovalCard'
 
 const App=()=>{
     return <div className="ui container comments">
-       <CommentDetail author="Brian Pondi" timeAgo="Today at 11:00 AM" avatar ={faker.image.avatar()} comment="Awesome content"/>
-       <CommentDetail author="Faith Muchembi" timeAgo="Today at 10:40 AM" avatar ={faker.image.avatar()} comment="Nice blog" />
-       <CommentDetail author="Mohammed Mwinyi" timeAgo="Yesterday at 03:00 PM" avatar ={faker.image.avatar()} comment="I love it"/>
+    <ApprovalCard>
+       <CommentDetail 
+        author="Brian Pondi" 
+        timeAgo="Today at 11:00 AM" 
+        avatar ={faker.image.avatar()} 
+        comment="Awesome content"
+        />
+    </ApprovalCard>
+
+    <ApprovalCard>
+       <CommentDetail 
+        author="Faith Muchembi" 
+        timeAgo="Today at 10:40 AM" 
+        avatar ={faker.image.avatar()} 
+        comment="Nice blog" 
+        />
+    </ApprovalCard>
+    
+    <ApprovalCard>
+       <CommentDetail 
+        author="Mohammed Mwinyi" 
+        timeAgo="Yesterday at 03:00 PM" 
+        avatar ={faker.image.avatar()} 
+        comment="I love it"/>
+    </ApprovalCard>
     </div>
 };
 
