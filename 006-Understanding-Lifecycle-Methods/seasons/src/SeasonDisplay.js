83ca0a707df22ceab7764fa74ce8a94fa28b4eec
PondiB <pondibrian@gmail.com>
2020-10-30 13:59:30 +0100

    :palm_tree: Default props

--- 006-Understanding-Lifecycle-Methods/seasons/src/SeasonDisplay.js
+++ 006-Understanding-Lifecycle-Methods/seasons/src/SeasonDisplay.js
@@ -1,4 +1,17 @@
+import './SeasonDisplay.css'
 import React from 'react';
+
+const seasonConfig ={
+    summer:{
+        text: "Let's hit the Beach",
+        iconName: "sun"
+    },
+    winter:{
+        text: "Burr, It is cold",
+        iconName: "snowflake"
+
+    }
+}
 
 const getSeason = (lat, month) =>{
     if (month >2 && month <9){
@@ -11,10 +24,14 @@ const getSeason = (lat, month) =>{
 
 const SeasonDisplay =(props) =>{
     const season = getSeason(props.lat, new Date().getMonth())
-    const text = season ==='winter'?'Burr, It is Chilly ': 'Lets hit the Beach';
+ //   const text = season ==='winter'?'Burr, It is Chilly ': 'Lets hit the Beach';
+ //   const icon = season ==='winter'?'snowflake ': 'sun';
+    const {text, iconName} =seasonConfig[season];
     return(
-        <div>
-            {text}
+        <div className={`season-display ${season}`}> 
+            <i className={`icon-left massive ${iconName} icon`}/>
+            <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`}/>
         </div>
     );
 };
