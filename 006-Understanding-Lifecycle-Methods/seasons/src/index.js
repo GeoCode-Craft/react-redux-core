a9d8a6cc1251dcb4bb906fd31ce53ab70a9b1df3
PondiB <pondibrian@gmail.com>
2020-10-30 12:17:42 +0100

    :snowflake: React Lifecyle and passing state as props

---
+++ 006-Understanding-Lifecycle-Methods/seasons/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
+
+
+class App extends React.Component{
+
+ // constructor(props){
+ //   super(props);
+    //This is the only time we do direct assignment to this.state
+ //   this.state = {lat: null, erorMessage: ''};  
+  //}
+
+   state = {lat: null, erorMessage: ''};  
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+        //callback function
+        position => this.setState({lat: position.coords.latitude}),
+        err =>this.setState({errorMessage: err.message})
+    );
+
+  }
+
+    //React says we have to deifine render
+    render() { 
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error:{this.state.errorMessage}</div>;
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat}/>;
+        }
+
+        return <div>Loading...</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"))
