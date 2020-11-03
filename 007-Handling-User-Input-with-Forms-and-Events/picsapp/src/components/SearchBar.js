bfe5bc399b570e96e642c3e507befb17bd18f416
PondiB <pondibrian@gmail.com>
2020-11-03 22:23:16 +0100

    :art: Invoking callbacks in Children

--- 007-Handling-User-Input-with-Forms-and-Events/picsapp/src/components/SearchBar.js
+++ 007-Handling-User-Input-with-Forms-and-Events/picsapp/src/components/SearchBar.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(event){
-       console.log(event.target.value);
-    }
+    state = {term: ' '};
 
- //   onInputClick(){
-//       console.log("Input was clicked");
-//  }
+
+    onFormSubmit=(event)=>{
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
 
     render() {
         return(
         <div className="ui segment">
-            <form className="ui form">
+            <form onSubmit={this.onFormSubmit} className="ui form">
              <div className="field">
                 <label>Image Search</label>
                 <input type="text" 
                 //Alt: onChange={event =>console.log(event.target.value)}
-                onChange={this.onInputChange} />
+                //onChange={this.onInputChange}
+                value ={this.state.term}
+                onChange={e=> this.setState({term: e.target.value})} />
              </div>   
             </form>
         </div>);
