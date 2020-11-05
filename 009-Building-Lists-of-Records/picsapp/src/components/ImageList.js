fb8b56677e0fea9944019e321499d3051095c4b1
PondiB <pondibrian@gmail.com>
2020-11-05 23:52:46 +0100

    :microphone: Implementing Keys in Lists improves performance

--- 009-Building-Lists-of-Records/picsapp/src/components/ImageList.js
+++ 009-Building-Lists-of-Records/picsapp/src/components/ImageList.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const ImageList =(props)=>{
-    const images = props.images.map((image)=>{
-        return <img src={image.urls.regular} alt="Images here"/>
+    //const images = props.images.map((image)=>{
+    //Destructuring for properties we care about
+    const images = props.images.map(({description, id, urls})=>{
+        return <img key={id} src={urls.regular} alt={description} />;
     });
     return <div>{images}</div>
 };
