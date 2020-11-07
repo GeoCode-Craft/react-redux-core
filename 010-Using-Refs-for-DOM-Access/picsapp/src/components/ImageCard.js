012fa47d84937f599c8cff5b0fc6503aae8621e3
PondiB <pondibrian@gmail.com>
2020-11-08 00:00:59 +0100

    :foggy: Dynamic Spans

--- 010-Using-Refs-for-DOM-Access/picsapp/src/components/ImageCard.js
+++ 010-Using-Refs-for-DOM-Access/picsapp/src/components/ImageCard.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.imageRef = React.createRef();
+
+        this.state = {spans:0};
+    }
+    componentDidMount(){
+        this.imageRef.curent.addEventListener('load', this.setSpans);
+  
+    }
+    setSpans=()=>{
+        const height =this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height/ 10);
+
+        this.setState({spans});
+    }
+
     render() {
         const {description, urls} = this.props.image;
         return (
-            <div className="">
+            <div style={{gridRowEnd: `span ${this.state.spans}`}}>
                 <img 
+                ref ={this.imageRef}
                 src={urls.regular}
                 alt={description} />
             </div>
