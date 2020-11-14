fb5710076ed6a3aea4f71bc1a957671beaaa89b3
PondiB <pondibrian@gmail.com>
2020-11-14 23:39:32 +0100

    :tada: Set up of base api url

--- 011-Lets-Test-Your-React-Mastery/videos/src/apis/youtube.js
+++ 011-Lets-Test-Your-React-Mastery/videos/src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyASKBRGsexI2HHtz8F9FdoCz7PkH5cu8qs';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults:5,
+        key: KEY
+    }
+});
+
