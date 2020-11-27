d96af0f260ea13eeb2d5433571fa80a71aeee253
PondiB <pondibrian@gmail.com>
2020-11-27 22:28:03 +0100

    :alien: fixed warnings and added defalt video

--- 011-Lets-Test-Your-React-Mastery/videos/src/components/VideoList.js
+++ 011-Lets-Test-Your-React-Mastery/videos/src/components/VideoList.js
@@ -4,7 +4,14 @@ import VideoItem from './VideoItem';
 const VideoList=({ videos, onVideoSelect }) =>{
     //props.videos destructured above
    const renderedList = videos.map( video => {
-        return <VideoItem  onVideoSelect={onVideoSelect} video={video}/>;
+        return ( 
+        <VideoItem  
+            key={video.id.videoId} 
+            onVideoSelect={onVideoSelect} 
+            video={video}
+
+        />
+     );
     });
     return <div className="list-group">{renderedList}</div>
 };
